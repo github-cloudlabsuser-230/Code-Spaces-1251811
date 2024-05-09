1d0e3130bed3c1cd0bff14cc572678d655e126f1
github-cloudlabsuser-230 <153943652+github-cloudlabsuser-230@users.noreply.github.com>
2024-05-09 07:03:24 +0000

    Copilot commit

---
+++ test.js
@@ -0,0 +1,45 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter the first number: ', (firstNumber) => {
+    rl.question('Enter the operator (+, -, *, /): ', (operator) => {
+        rl.question('Enter the second number: ', (secondNumber) => {
+            let result;
+
+            firstNumber = Number(firstNumber);
+            secondNumber = Number(secondNumber);
+
+            switch (operator) {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    if (secondNumber !== 0) {
+                        result = firstNumber / secondNumber;
+                    } else {
+                        console.log('Error! Division by zero is not allowed.');
+                        rl.close();
+                        return;
+                    }
+                    break;
+                default:
+                    console.log('Invalid operator!');
+                    rl.close();
+                    return;
+            }
+
+            console.log(`The result is: ${result}`);
+            rl.close();
+        });
+    });
+});
